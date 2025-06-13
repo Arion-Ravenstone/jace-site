94f776e3a97e27df44517af33f78d69da2b68673
Arion Ravenstone <ariontrh@gmail.com>
2025-06-13 02:46:25 -0300

    updating files
    updating header, with new nav and styles

--- src/App.jsx
+++ src/App.jsx
@@ -19,15 +19,18 @@ const App = () => {
               onError={(e) => { e.target.onerror = null; e.target.src="https://placehold.co/48x48/cccccc/333333?text=Error"; }}
             />
             {/* Website Name */}
-            <h1 className="website-name">My Awesome Website</h1>
+            <h1 className="website-name">J. Murphy</h1>
           </div>
 
           {/* Navigation Links */}
           <nav className="main-nav">
             <ul className="nav-list">
               <li className="nav-item"><a href="#" className="nav-link">Home</a></li>
-              <li className="nav-item"><a href="#" className="nav-link">About</a></li>
-              <li className="nav-item"><a href="#" className="nav-link">Services</a></li>
+              <li className="nav-item"><a href="#" className="nav-link">About J. Murphy</a></li>
+              <li className="nav-item"><a href="#" className="nav-link">Books</a></li>
+              <li className="nav-item"><a href="#" className="nav-link">Blog</a></li>
+              <li className="nav-item"><a href="#" className="nav-link">Portfolio</a></li>
+              <li className="nav-item"><a href="#" className="nav-link">Testimonials</a></li>
               <li className="nav-item"><a href="#" className="nav-link">Contact</a></li>
             </ul>
           </nav>
